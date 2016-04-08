a32cbaa233e79f2682df7fd75f9508cb18adad89
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-04-07 23:14:46 -0500

    added more links and finished all needed for Library Menu page

--- main.rb
+++ main.rb
@@ -21,7 +21,27 @@ end
 
 get '/libraries/list_of_libraries' do
   erb :list_of_libraries
+end
 
+get '/libraries/list_of_staff_members' do
+  erb :list_of_libraries
+end
+
+get '/libraries/reassign_staff_member' do
+  erb :reassign_staff_member
+end
+
+get '/libraries/reassign_book' do
+  erb :reassign_book
+end
+
+get '/libraries/add_new_library' do
+  erb :add_new_library
+end
+
+get '/libraries/edit_library' do
+  erb :edit_library
+end
 
 #   Books                 ############################### Books
 get '/books' do
@@ -31,6 +51,11 @@ end
 get '/books/list_of_books' do
   erb :list_of_books
 end
+
+get '/books/check_out_books' do
+  erb :check_out_books
+end
+
 
 #   Staff Members         ############################### Staff Members
 get '/staff_members' do
