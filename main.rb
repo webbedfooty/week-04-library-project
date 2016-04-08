d5421bc0b8bfc9ed0f91ab58571a33b70b4de128
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-04-07 22:11:40 -0500

    Added in more links to the Library Menu page

--- main.rb
+++ main.rb
@@ -14,18 +14,30 @@ get '/' do
   erb :main_menu
 end
 
+#   Libraries             ############################### Libraries
 get '/libraries' do
   erb :libraries_menu
 end
 
+get '/libraries/list_of_libraries' do
+  erb :list_of_libraries
+
+
+#   Books                 ############################### Books
 get '/books' do
   erb :books_menu
 end
 
+get '/books/list_of_books' do
+  erb :list_of_books
+end
+
+#   Staff Members         ############################### Staff Members
 get '/staff_members' do
   erb :staff_members_menu
 end
 
+#   Patrons               ############################### Patrons
 get '/patrons' do
   erb :patrons_menu
 end
